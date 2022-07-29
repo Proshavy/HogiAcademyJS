bbad2c1da6bfae63b34637367e7c9fb99893c883
Prosper Havyarimana <proshavy@gmail.com>
2022-07-29 14:39:27 +0200

    Ajout Maximum et Minimum code

---
+++ minimum-maximum.js
@@ -0,0 +1,14 @@
+//Le Maximum et le Minimum
+let a = 10;
+let b = 20;
+let c = 30;
+
+if(a>b && b>c){
+    console.log("Le maximum est A");
+}else if(a<b && b>c){
+    console.log("Le maximum est B");
+}else if(a>b && b<c){
+    console.log("Le maximum est A");
+}else{
+    console.log("Le maximum est C");
+}
